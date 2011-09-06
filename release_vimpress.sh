0afe8b4300b34263c696e38b0beafc1d7882ad24
Preston M. <pentie@gmail.com>
2011-09-06 13:02:29 +0800

    Update release script to include branch name in release file.

--- release_vimpress.sh
+++ release_vimpress.sh
@@ -3,11 +3,12 @@
 
 TEMP_DIR=/tmp/vimpress_relase
 REV=`hg log -r tip|grep -e "^changeset"|awk '{print $2}'|tr ':' '_'`
+BRANCH=`hg branch`
 hg archive $TEMP_DIR
 cd $TEMP_DIR/plugin
 (sed 's/^python.\+$/python << EOF/' blog-dev.vim; cat blog.py) > blog.vim
 VER=`grep Version blog.vim|awk '{print $3}'`
-RELEASE_FILE="/tmp/vimpress_"$VER"_r"$REV".zip"
+RELEASE_FILE="/tmp/"$BRANCH"_"$VER"_r"$REV".zip"
 cd $TEMP_DIR
 if [[ -f $RELEASE_FILE ]]; then rm $RELEASE_FILE; fi
 zip -x '.hgtags' -x '.hg_archival.txt' -x release_vimpress.sh -x plugin/blog.py -x plugin/blog-dev.vim -r $RELEASE_FILE .
